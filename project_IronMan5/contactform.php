7794d5e78914486c241e13a059cab7d75e48a183
Weicheng Hu <wc688597@dal.ca>
2021-11-18 19:38:37 -0400

    add all sections in contact form

--- project_IronMan5/contactform.php
+++ project_IronMan5/contactform.php
@@ -2,6 +2,24 @@
     require_once "includes/header.php";
 ?>
 
+<div id="contact-form">
+    <h1>Contact us</h1>
+    <label>Name:</label>
+    <input type="text" name="name">
+    <label>E-mail address:</label>
+    <input type="text" name="e-mail">
+    <label>Message:</label>
+    <textarea name="message"></textarea>
+</div>
+
+<div id="contacu-tips">
+
+</div>
+
+<div id="other-contact">
+    
+</div>
+
 <?php
     require_once "includes/footer.php";
 ?>
