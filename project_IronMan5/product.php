b94d2d7edafba8f32b2272af8588dd74019dc757
Weicheng Hu <wc688597@dal.ca>
2021-11-18 19:31:59 -0400

    add all sections in product page

--- project_IronMan5/product.php
+++ project_IronMan5/product.php
@@ -2,6 +2,31 @@
     require_once "includes/header.php";
 ?>
 
+<div id="product-nav">
+
+</div>
+
+<div id="product-intro-img">
+    <img>
+    <h1></h1>
+</div>
+
+<div id="product-imgs">
+
+</div>
+
+<div id="product-features">
+
+</div>
+
+<div id="product-specs">
+
+</div>
+
+<div id="reviews">
+    
+</div>
+
 <?php
     require_once "includes/footer.php";
 ?>
