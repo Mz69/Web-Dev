cab12acfb0878533c1924448c4821a749d3c3c66
Weicheng Hu <wc688597@dal.ca>
2021-11-27 12:08:53 -0400

    add content

--- project_IronMan5/product.php
+++ project_IronMan5/product.php
@@ -1,22 +1,64 @@
 <?php
     require_once "includes/header.php";
+    require_once "db/db.php";
 ?>
 
 <div id="product-nav">
-
+    <ul>
+        <li><a><img></a></li>
+        <li><a href="#product_intro_img">Overview</a></li>
+        <li><a href="#product-features">Features</a></li>
+        <li><a href="#product-specs">Specs</a></li>
+        <li><a href="#reviews">Reviews</a></li>
+        <li><a>Cart</a></li>
+    </ul>
 </div>
 
 <div id="product-intro-img">
-    <img>
-    <h1></h1>
+<?php
+    $product_id = 1;
+    $sql = "SELECT * FROM iron_product WHERE i_product_id = $product_id;";
+
+    $result = $dbconn -> query($sql);
+    while($row=$result->fetch_assoc()) {
+        echo "<h1>" . $row['i_product_name'] . "</h1>";
+    }
+
+    $sql = "SELECT * FROM iron_product_imgs WHERE i_product_id = $product_id;";
+    $result = $dbconn -> query($sql);
+    while($row=$result->fetch_assoc()) {
+        echo "<img src='img/" . $row['i_img_path'] . "' id='big-img'>";
+        break;
+    }
+?>
 </div>
 
 <div id="product-imgs">
+    <ul>
+        <?php
+            $product_id = 1;
+            $sql = "SELECT * FROM iron_product_imgs WHERE i_product_id = $product_id;";
+            $result = $dbconn -> query($sql);
+            while($row=$result->fetch_assoc()) {
+                echo "<li><img src='img/" . $row['i_img_path'] . "' class='samll-img'></li>";
+            }
+        ?>
+    </ul>
 
+    <button id="left"><</button>
+    <button id="right">></button>
 </div>
 
 <div id="product-features">
+    <?php
+        $product_id = 1;
+        $sql = "SELECT * FROM iron_product WHERE i_product_id = $product_id;";
+        $result = $dbconn -> query($sql);
 
+        while($row=$result->fetch_assoc()) {
+            echo "<p>" . $row['i_product_desc'] . "</p>";
+        }
+    ?>
 </div>
 
 <div id="product-specs">
