e569d60ec58856c1176da2c08bcb7d0ec88f8500
Weicheng Hu <wc688597@dal.ca>
2021-11-28 18:05:56 -0400

    Contact form is almost done

--- project_IronMan5/contactform.php
+++ project_IronMan5/contactform.php
@@ -2,23 +2,43 @@
     require_once "includes/header.php";
 ?>
 
-<div id="contact-form">
-    <h1>Contact us</h1>
-    <label>Name:</label>
-    <input type="text" name="name">
-    <label>E-mail address:</label>
-    <input type="text" name="e-mail">
-    <label>Message:</label>
-    <textarea name="message"></textarea>
+<link rel="stylesheet" href="css/contactForm.css">
+
+<div id="content">
+
+    <div id="contact-form">
+        <h1>Contact us</h1>
+        <form method="POST">
+            <div class="form-row">
+                <label>Name:</label>
+                <input type="text" name="name">
+            </div>
+
+            <div class="form-row">
+                <label>E-mail address:</label>
+                <input type="text" name="e-mail">
+            </div>
+
+            <div class="form-row">
+                <label>Message:</label>
+                <textarea name="message"></textarea>
+            </div>
+
+            <div>
+                <input type="submit" id="submit-contact" value="Submit">
+            </div>
+        </form>
+    </div>
+
+    <div id="other-contact">
+        <h1>Other way to contact us</h1>
+        
+        <p>Office address: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+        <p>Office telephone number: xxxxxxxxxx</p>
+    </div>
 </div>
 
-<div id="contacu-tips">
-
-</div>
-
-<div id="other-contact">
-    
-</div>
+<script src="js/script.js"></script>
 
 <?php
     require_once "includes/footer.php";
