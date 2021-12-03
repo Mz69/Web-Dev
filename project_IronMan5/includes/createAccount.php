b5293b83234b4bd42309abd894206ed76862cea2
Ted Graveson <ted.graveson@dal.ca>
2021-12-03 14:13:23 -0400

    More fuctionality implemented

--- project_IronMan5/includes/createAccount.php
+++ project_IronMan5/includes/createAccount.php
@@ -13,21 +13,52 @@
     }
     else {
 ?>
-<form method="POST" action="includes/createAccount.php">
-  <div class="form-group">
-    <label for="exampleInputPassword1">Name</label>
-    <input name="name" type="text" class="form-control" id="exampleInputPassword1" placeholder="Full Name">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<section id="create-account" class="row">
+  <form method="POST" action="includes/createAccount.php" class="col-8 m-auto">
+    <div class="row">
+      <div class="form-group">
+        <label for="exampleInputPassword1">Name</label>
+        <input name="name" type="text" class="form-control" id="exampleInputPassword1" placeholder="Full Name">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    
+    <div class="form-group">
+      <label for="country">Country</label>
+      <select class="custom-select d-block w-100" id="country" required>
+        <option value="">Choose...</option>
+        <option>United States</option>
+      </select>
+      <div class="invalid-feedback">
+        Please select a valid country.
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="state">State</label>
+      <select class="custom-select d-block w-100" id="state" required>
+        <option value="">Choose...</option>
+        <option>California</option>
+      </select>
+      <div class="invalid-feedback">
+        Please provide a valid state.
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="zip">Zip</label>
+      <input type="text" class="form-control" id="zip" placeholder="" required>
+      <div class="invalid-feedback">
+        Zip code required.
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</section>
 <?php
     }
 ?>
