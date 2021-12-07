f84017b34def81e320ff5a43d9404c9f29648004
Weicheng Hu <wc688597@dal.ca>
2021-12-07 13:55:41 -0400

    add link to ccontact  form in product page

--- project_IronMan5/product.php
+++ project_IronMan5/product.php
@@ -11,7 +11,7 @@
         <a href="#product-features">Features</a>
         <a href="#product-specs">Specs</a>
         <a href="#reviews">Reviews</a>
-        <a>Cart</a>
+        <a href="contactform.php">Contact us</a>
     </div>
 
     <body>
@@ -99,7 +99,7 @@
             </div>
 
             <div id="add2cart">
-
+                
                 <div id="num-control">
                     <button onclick="numChange(-1)">-</button>
                     <input id="num-purchase" value="1">
